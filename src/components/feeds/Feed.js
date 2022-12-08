5b603a8d36ed979ae27dc708a9625863d1b28d69
ignatius <ignatius.mafahla@dadevs.co.za>
2022-12-08 16:25:59 +0200

    firebase problems 19:35

--- src/components/feeds/Feed.js
+++ src/components/feeds/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./feed.css"
 import { Avatar } from "@material-ui/core"
 import PhotoIcon from '@material-ui/icons/Photo';
@@ -6,17 +6,46 @@ import YouTubeIcon from '@material-ui/icons/YouTube';
 import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
 import TodayIcon from '@material-ui/icons/Today';
 import AssignmentIcon from '@material-ui/icons/Assignment';
-import Post from '../post/Post'
-
+import Post from '../post/Post';
+import firebase from "./firebase";
+// import * as firebase from "firebase/app"
+import { db } from './firebase';
 
 function Feed() {
+
+const [posts,setPost] = useState([]);
+const[input,setInput] = useState();
+
+const submitPost=(e)=>{
+  
+    e.preventDefault();
+    db.collection("posts").add({
+        name: "mafahla",
+        description:"kjghghjikoihghjk",
+        message:input,
+        photoUrl:"https://play-lh.googleusercontent.com/kHRf85euDvW-Kg7ThXK2vv-J-Yye9uxoo6GQvUcAwudNRz1sQvXubAl_m2bu6KJofA",
+        timestamp:firebase.firestore.fieldValue.serverTimestamp()
+    })
+    setInput(" ")
+}
+useEffect(()=>{
+    db.collection("posts").orderBy("timestamp","desc").onSnapShot(snapshot =>{
+        setPost(snapshot.docs.map(doc=>({
+            id:doc.id,
+            data:doc.data()
+        })))
+    })
+},[])
+
+console.log(posts);
+
     return (
         <div className='feed'>
             <div className='feed__input'>
                 <div className='feed__form'>
                 <Avatar />
-                <form>
-                    <input type="text" placeholder="Start a post" />
+                <form onSubmit={submitPost}>
+                    <input type="text" placeholder="Start a post" value={input} onChange={e=>setInput(e.target.value)}/>
                     <input type="submit" />
                 </form>
                 </div>
@@ -59,26 +88,6 @@ function Feed() {
                     transform are catalysts for thriving in their careers." 
             photoUrl="https://cdn.pixabay.com/photo/2018/07/31/22/08/lion-3576045__480.jpg"/>
             
-            <Post name="Microsoft Developers" description="Promoted" message="Get Azure with no up-front commitment. Pay only for the services you use beyond free amounts." 
-            photoUrl="https://play-lh.googleusercontent.com/kHRf85euDvW-Kg7ThXK2vv-J-Yye9uxoo6GQvUcAwudNRz1sQvXubAl_m2bu6KJofA"/>
-            
-
-            <Post name="Ignatius Mafahla" description="this is a clone test" message=" At FNB, we are committed to realising your potential. 
-                    Changeables understand that opportunities to learn, grow, and 
-                    transform are catalysts for thriving in their careers." 
-            photoUrl="https://cdn.pixabay.com/photo/2018/07/31/22/08/lion-3576045__480.jpg"/>
-            
-            <Post name="Microsoft Developers" description="Promoted" message="Get Azure with no up-front commitment. Pay only for the services you use beyond free amounts." 
-            photoUrl="https://play-lh.googleusercontent.com/kHRf85euDvW-Kg7ThXK2vv-J-Yye9uxoo6GQvUcAwudNRz1sQvXubAl_m2bu6KJofA"/>
-            
-            <Post name="Ignatius Mafahla" description="this is a clone test" message=" At FNB, we are committed to realising your potential. 
-                    Changeables understand that opportunities to learn, grow, and 
-                    transform are catalysts for thriving in their careers." 
-            photoUrl="https://cdn.pixabay.com/photo/2018/07/31/22/08/lion-3576045__480.jpg"/>
-            
-            
-            <Post name="Microsoft Developers" description="Promoted" message="Get Azure with no up-front commitment. Pay only for the services you use beyond free amounts." 
-            photoUrl="https://play-lh.googleusercontent.com/kHRf85euDvW-Kg7ThXK2vv-J-Yye9uxoo6GQvUcAwudNRz1sQvXubAl_m2bu6KJofA"/>
             
 
 
---
+++ src/components/feeds/firebase.js
@@ -0,0 +1,31 @@
+// import firebase from 'firebase'
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore/lite";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBRopKhIDNvZ2RVAb1XXnvJP5f5EasqwdY",
+    authDomain: "linkedin-clone-mafahla.firebaseapp.com",
+    projectId: "linkedin-clone-mafahla",
+    storageBucket: "linkedin-clone-mafahla.appspot.com",
+    messagingSenderId: "541732279356",
+    appId: "1:541732279356:web:953287c1e311a5a0ec1f39"
+  };
+
+  //this line connects everything
+  // const firebaseApp = firebase.intializeApp(firebaseConfig);
+
+  // gets the firebase database
+  // const db = firebaseApp.firestore();
+
+  // export { db }
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth();
+const storage = getStorage(app);
+//const provider=new GoogleAuthProvider()
+
+export { db, auth, storage };
